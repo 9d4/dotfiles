4d4a13173522b784f219539c45a0c4f5979170dd
dimanda <d@dimanda.my.id>
2025-07-12 05:00:09 +0700

    init

---
+++ .config/nvim/lua/dimanda/init.lua
@@ -0,0 +1,8 @@
+-- Disable netrw
+vim.g.loaded_netrwPlugin = 1
+vim.g.loaded_netrw = 1
+
+require("dimanda.set")
+require("dimanda.remap")
+require("dimanda.statusline")
+require("dimanda.lazy_init")
