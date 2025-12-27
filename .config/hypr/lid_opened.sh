ac4bc25d3c5bac9e8a4311a8d540bd807ab519c0
dimanda <d@dimanda.dev>
2025-12-27 17:12:46 +0700

    add on lid opened or closed event trigger

--- .config/hypr/lid_opened.sh
+++ .config/hypr/lid_opened.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# Get connected monitors
 CONNECTED_MONITORS=$(hyprctl monitors | grep "Monitor" | awk '{print $2}')
 
-# Define your internal and external monitor names (adjust as needed, use `hyprctl monitors` to find yours)
 INTERNAL_MONITOR="eDP-1" 
 EXTERNAL_MONITOR="HDMI-A-1"
 
-# Check if external monitor is connected
+# If external monitor is connected, then disable internal monitor, otherwise suspend
 if echo "$CONNECTED_MONITORS" | grep "$EXTERNAL_MONITOR"; then
-    # External connected: disable internal
     hyprctl keyword monitor "$INTERNAL_MONITOR",disable
     hyprctl keyword monitor "$EXTERNAL_MONITOR",highrr,auto,auto # Example config for external
+else
+    # back to normal
+    hyprctl reload
 fi
