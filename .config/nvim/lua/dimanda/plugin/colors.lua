87193a2b76e279ce6f81a6743039bfb698ad9d7e
dimanda <d@dimanda.dev>
2026-01-07 23:06:05 +0700

    update nvim treesitter and tmux status bar

--- .config/nvim/lua/dimanda/plugin/colors.lua
+++ .config/nvim/lua/dimanda/plugin/colors.lua
@@ -1,22 +1,23 @@
 return {
+	enabled = true,
 	"Shatur/neovim-ayu",
 	config = function()
 		require("ayu").setup({
 			mirage = false, -- Set to `true` to use `mirage` variant instead of `dark` for dark background.
-			terminal = true, -- Set to `false` to let terminal manage its own colors.
-			-- overrides = {
-			-- 	Normal = { bg = "None" },
-			-- 	NormalFloat = { bg = "none" },
-			-- 	-- ColorColumn = { bg = "None" },
-			-- 	SignColumn = { bg = "None" },
-			-- 	Folded = { bg = "None" },
-			-- 	FoldColumn = { bg = "None" },
-			-- 	CursorLine = { bg = "None" },
-			-- 	CursorColumn = { bg = "None" },
-			-- 	VertSplit = { bg = "None" },
-			-- }, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
+			terminal = false, -- Set to `false` to let terminal manage its own colors.
+			overrides = {
+				Normal = { bg = "None" },
+				NormalFloat = { bg = "none" },
+				-- ColorColumn = { bg = "None" },
+				SignColumn = { bg = "None" },
+				Folded = { bg = "None" },
+				FoldColumn = { bg = "None" },
+				CursorLine = { bg = "None" },
+				CursorColumn = { bg = "None" },
+				VertSplit = { bg = "None" },
+			}, -- A dictionary of group names, each associated with a dictionary of parameters (`bg`, `fg`, `sp` and `style`) and colors in hex.
 		})
 
-		vim.cmd.colorscheme("ayu")
+		vim.cmd.colorscheme("ayu-mirage")
 	end,
 }
