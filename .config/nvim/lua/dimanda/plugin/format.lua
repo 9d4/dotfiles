e66a1bd8c8476fb3f97bb7197c1d2e98cdfed88d
dimanda <d@dimanda.dev>
2026-05-11 14:30:51 +0700

    add disable/enable format on save also format selection

--- .config/nvim/lua/dimanda/plugin/format.lua
+++ .config/nvim/lua/dimanda/plugin/format.lua
@@ -25,6 +25,8 @@ return {
 			typescript = { "prettierd", "prettier", stop_after_first = true },
 			typescriptreact = { "prettierd", "prettier", stop_after_first = true },
 			javascriptreact = { "prettierd", "prettier", stop_after_first = true },
+			toml = { "prettierd", "prettier", stop_after_first = true },
+			pug = { "prettierd", "prettier", stop_after_first = true },
 			json = { "prettier", stop_after_first = true },
 			go = { "gofumpt" },
 		},
@@ -33,7 +35,14 @@ return {
 			lsp_format = "fallback",
 		},
 		-- Set up format-on-save
-		format_on_save = { timeout_ms = 1000 },
+		format_on_save = function(bufnr)
+			-- Disable with a global or buffer-local variable
+			if vim.g.disable_autoformat or vim.b[bufnr].disable_autoformat then
+				return
+			end
+			return { timeout_ms = 1000, lsp_format = "fallback" }
+		end,
+
 		-- Customize formatters
 		formatters = {
 			shfmt = {
@@ -41,8 +50,39 @@ return {
 			},
 		},
 	},
+
 	init = function()
 		-- If you want the formatexpr, here is the place to set it
 		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+
+		vim.api.nvim_create_user_command("FormatDisable", function(args)
+			if args.bang then
+				-- FormatDisable! will disable formatting just for this buffer
+				vim.b.disable_autoformat = true
+			else
+				vim.g.disable_autoformat = true
+			end
+		end, {
+			desc = "Disable autoformat-on-save",
+			bang = true,
+		})
+
+		vim.api.nvim_create_user_command("FormatEnable", function()
+			vim.b.disable_autoformat = false
+			vim.g.disable_autoformat = false
+		end, {
+			desc = "Re-enable autoformat-on-save",
+		})
+
+		vim.keymap.set("", "<leader>f", function()
+			require("conform").format({ async = true }, function(err)
+				if not err then
+					local mode = vim.api.nvim_get_mode().mode
+					if vim.startswith(string.lower(mode), "v") then
+						vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Esc>", true, false, true), "n", true)
+					end
+				end
+			end)
+		end, { desc = "Format code" })
 	end,
 }
